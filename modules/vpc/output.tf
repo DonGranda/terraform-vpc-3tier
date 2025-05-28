8754ecb6f2622532a33abcb4f6623f5b0d2ab47e
Leslie Amegbor <lesliekofiameg@gmail.com>
2025-05-28 23:06:10 +0000

    igw and natgw output were added and the eip resource was editted

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -9,3 +9,17 @@ output "vpc_cidr_block" {
   value       = aws_vpc.main.cidr_block
   
 }
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+  
+}
+
+output "nat_gateway_ids" {
+  description = "A map of NAT Gateway IDs by subnet"
+  value       = [
+    for nat-gw in aws_aws_nat_gateway.vpc_nat_gateway:
+    nat-gw.id 
+  ]  
+}
--- modules/vpc/resource.tf
+++ modules/vpc/resource.tf
@@ -2,7 +2,7 @@ resource "aws_eip" "nat_gateway_eip" {
   for_each = {
     for subnet in aws_subnet.subnets:
     subnet.value.name => subnet.value
-    if subnet.value.tags.Type== "public"
+    if subnet.value.type== "public"
   }
   tags = {
     Name = "${var.name_prefix}-nat-gateway-eip"
