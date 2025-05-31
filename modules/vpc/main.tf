9a2456734cf71f3ac2ca91e2647ccc523c6f4b41
Leslie Amegbor <lesliekofiameg@gmail.com>
2025-05-31 16:01:12 +0000

    tags were added to the main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,16 +1,12 @@
 # Create the main VPC
 resource "aws_vpc" "main" {
-  # Set the CIDR block for the VPC
   cidr_block = var.vpc_cidr
   
-  # Enable DNS resolution for instances in VPC
   enable_dns_support = var.enable_dns_support
   
-  # Enable DNS hostnames for instances in VPC
   enable_dns_hostnames = var.enable_dns_hostnames
   
-  # Tag the VPC with name and environment
-  tags = {
+    tags = {
     Name        = "${var.project_name}-${var.environment}-vpc"
     Environment = var.environment
     Project     = var.project_name
