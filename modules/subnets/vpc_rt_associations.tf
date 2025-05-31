8137e0a2f0d8a2f6b2337adee631262e5d067b52
Leslie Amegbor <lesliekofiameg@gmail.com>
2025-05-31 18:02:55 +0000

    public association has been configed for public_subnet 1&2 to use one table

--- modules/subnets/vpc_rt_associations.tf
+++ modules/subnets/vpc_rt_associations.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table_association" "public_rt_association" {
  for_each = local.public_subnets
  subnet_id = aws_subnet.all_subnets[each.key].id
- route_table_id = aws_route_table.public_rt[each.key].id
+ route_table_id = aws_route_table.public_rt.id
 }
 
 #================= Private Route Table Associations ==============
