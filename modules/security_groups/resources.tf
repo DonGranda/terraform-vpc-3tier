798419c6a31f5e658537440da5153297a979c799
Leslie Amegbor <lesliekofiameg@gmail.com>
2025-05-31 23:56:29 +0000

    main.tf and resource swaped properly for easy identification

--- modules/security_groups/resources.tf
+++ modules/security_groups/resources.tf
@@ -1,22 +1,56 @@
-resource "aws_security_group" "public_sg" {
-    name=  "${var.name_prefix}-public-sg"
-    description = "Public security group for ${var.name_prefix}"
-    vpc_id = var.vpc_id
+resource "aws_vpc_security_group_ingress_rule" "public_ingress_rule" {
+  for_each = toset(["22", "80", "443"])
+  security_group_id = aws_security_group.web_sg.id
+  description        = "Allow HTTP traffic to public security group"
+  ip_protocol = "tcp"
+  from_port         = each.value
+  to_port           = each.value
+  cidr_ipv4         = "0.0.0.0/0"
   
 }
 
-
-resource "aws_security_group" "app-tier_sg" {
-    name=  "${var.name_prefix}-app-tier-sg"
-    description = "Private security group for the${var.name_prefix} app tier resources"
-    vpc_id = var.vpc_id
+resource "aws_vpc_security_group_egress_rule" "public_egress_rule" {
+  security_group_id = aws_security_group.web_sg.id
+  description        = "Allow HTTP traffic to public security group"
+  ip_protocol = -1 
+  cidr_ipv4         = "0.0.0.0/0"
   
 }
 
+###============== App Tier Security Group Rules ================
 
-resource "aws_security_group" "db_sg" {
-    name=  "${var.name_prefix}-DB-tier-sg"
-    description = "Private security group for ${var.name_prefix}"
-    vpc_id = var.vpc_id
-  
+resource "aws_vpc_security_group_ingress_rule" "app_ingress_rule" {
+  security_group_id = aws_security_group.app_sg.id
+  description        = "Allow HTTP traffic from the  public sg-group"
+  ip_protocol = "tcp"
+  from_port         = 8080
+  to_port           = 8080
+  referenced_security_group_id = aws_security_group.web_sg.id
 }
+
+resource "aws_vpc_security_group_egress_rule" "app_egress_rule" {
+  security_group_id = aws_security_group.app_sg.id
+  description        = "Allow traffic out of the app-tier security group"
+  ip_protocol = -1 
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+###============== DB Tier Security Group Rules ================
+
+resource "aws_vpc_security_group_ingress_rule" "db_ingress_rule" {
+  security_group_id = aws_security_group.db_sg.id
+  description        = "Allow MySQL traffic from the app-tier security group"
+  ip_protocol = "tcp"
+  from_port         = 3306 
+  to_port           = 3306
+  referenced_security_group_id = aws_security_group.app_sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "db_egress_rule" {
+  security_group_id = aws_security_group.db_sg.id
+  description        = "Allow all outbound traffic from the DB tier security group"
+  ip_protocol = -1 
+  cidr_ipv4         = "0.0.0.0/0"
+  }
+
+
