efcfcf1e172be1d7da0a95fac99165bb385e8be7
Leslie Amegbor <lesliekofiameg@gmail.com>
2025-06-03 23:36:05 +0000

    public route table was edited az tags removed as well at the each.key was removed from the Name tag

--- modules/subnets/vpc_rt.tf
+++ modules/subnets/vpc_rt.tf
@@ -1,11 +1,10 @@
 resource "aws_route_table" "public_rt" {
-  vpc_id   = var.vpc_id
+  vpc_id = var.vpc_id
 
   tags = {
-    Name        = "${var.project_name}-${var.environment}-${each.key}-public-rt"
+    Name        = "${var.project_name}-${var.environment}-public-rt"
     Environment = var.environment
     Project     = var.project_name
-    AZ          = each.value.availability_zone
     Type        = "Public"
   }
   lifecycle {
@@ -17,8 +16,9 @@ resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = var.internet_gateway_id
+  depends_on             = [aws_nat_gateway.main]
 }
-  
+
 #============== Private Route Tables ==============
 resource "aws_route_table" "private_rt" {
   for_each = local.private_route_tables
@@ -38,11 +38,11 @@ resource "aws_route_table" "private_rt" {
 }
 
 resource "aws_route" "private_route" {
-  for_each = local.private_route_tables
+  for_each               = local.private_route_tables
   route_table_id         = aws_route_table.private_rt[each.key].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.main["public-${each.key}"].id
-  
+
   depends_on = [aws_nat_gateway.main]
 }
 
