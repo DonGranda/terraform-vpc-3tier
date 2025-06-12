9c7fdc248bb82d3686695f276713fe7bb51c9274
DonGranda <lesliekofiameg@gmail.com>
2025-06-12 22:49:30 +0000

    subnet_ids was editted to use db_private_subnets

--- modules/db/resource.tf
+++ modules/db/resource.tf
@@ -1,11 +1,11 @@
 resource "aws_db_subnet_group" "db-subnet-grp" {
-    name = "${var.project_name}-${var.environment}-db-subnet-grp"
-    description = "Database subnet group for ${var.project_name} in ${var.environment} environment"
-    subnet_ids = var.db_private_subnets
-    tags = {
-        Name        = "${var.project_name}-${var.environment}-db-subnet-grp"
-        Environment = var.environment
-        Project     = var.project_name
-    }
-  
+  name        = "${var.project_name}-${var.environment}-db-subnet-grp"
+  description = "Database subnet group for ${var.project_name} in ${var.environment} environment"
+  subnet_ids  = var.db_private_subnets
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-db-subnet-grp"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+
 }
