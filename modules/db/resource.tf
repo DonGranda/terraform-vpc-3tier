5e23b86a12507da58d2f5f209e4caf8bb39e72cc
DonGranda <lesliekofiameg@gmail.com>
2025-06-12 20:08:15 +0000

    db subnet group has been added

---
+++ modules/db/resource.tf
@@ -0,0 +1,11 @@
+resource "aws_db_subnet_group" "db-subnet-grp" {
+    name = "${var.project_name}-${var.environment}-db-subnet-grp"
+    description = "Database subnet group for ${var.project_name} in ${var.environment} environment"
+    subnet_ids = var.private_subnet_ids
+    tags = {
+        Name        = "${var.project_name}-${var.environment}-db-subnet-grp"
+        Environment = var.environment
+        Project     = var.project_name
+    }
+  
+}
