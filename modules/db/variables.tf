a91fa339f263027852d595a6463ae3d49dbe6597
DonGranda <lesliekofiameg@gmail.com>
2025-06-19 18:27:57 +0000

    var was fmt

--- modules/db/variables.tf
+++ modules/db/variables.tf
@@ -8,25 +8,25 @@ variable "environment" {
 variable "private_subnet_ids" {
   description = "List of private subnet IDs for the database"
   type        = list(string)
-  
+
 }
 
 variable "db_private_subnets" {
   description = "Map of private subnets for the database"
-type = list(string)
-  
+  type        = list(string)
+
 }
 
 variable "db_engine" {
   description = "Database engine"
   type        = string
-  
+
   validation {
     condition = contains([
-      "mysql", "postgres", "mariadb", "oracle-ee", "oracle-se2", 
-      "oracle-se1", "oracle-se", "sqlserver-ee", "sqlserver-se", 
+      "mysql", "postgres", "mariadb", "oracle-ee", "oracle-se2",
+      "oracle-se1", "oracle-se", "sqlserver-ee", "sqlserver-se",
       "sqlserver-ex", "sqlserver-web"
-    ], var.engine)
+    ], var.db_engine)
     error_message = "Engine must be a valid RDS engine type."
   }
 }
@@ -34,7 +34,7 @@ variable "db_engine" {
 variable "db_engine_version" {
   description = "Database engine version"
   type        = string
-  
+
 }
 
 variable "db_instance_class" {
@@ -45,41 +45,46 @@ variable "db_instance_class" {
 variable "db_allocated_storage" {
   description = "Allocated storage for the database in GB"
   type        = number
-  
+
 }
 
 variable "db_max_allocated_storage" {
   description = "Maximum allocated storage for the database in GB"
   type        = number
-  
+
 }
 
 variable "db_storage_type" {
   description = "Storage type for the database"
   type        = string
-    #default     = "gp2"
-  
+  #default     = "gp2"
+
 }
 
 variable "db_storage_encrypted" {
   description = "Whether the database storage is encrypted"
   type        = bool
   default     = true
-  
+
 }
 
 variable "db_name" {
   description = "Name of the database"
   type        = string
-  
+
 }
 variable "db_username" {
   description = "Username for the database"
   type        = string
-  
+
 }
 variable "manage_master_user_password" {
   description = "Whether to manage the master user password"
   type        = bool
   default     = true
+}
+variable "db_security_group_id" {
+  description = "List of security group IDs for the database tier"
+  type        = list(string)
+
 }
