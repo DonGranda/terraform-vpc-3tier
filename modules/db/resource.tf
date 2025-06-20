78cc48f9a32349c895a10ee885fc468655372d77
DonGranda <lesliekofiameg@gmail.com>
2025-06-20 19:47:09 +0000

    subnet_id var was changed

--- modules/db/resource.tf
+++ modules/db/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_db_subnet_group" "db-subnet-grp" {
   name        = "${var.project_name}-${var.environment}-db-subnet-grp"
   description = "Database subnet group for ${var.project_name} in ${var.environment} environment"
-  subnet_ids  = var.private_subnet_ids
+  subnet_ids  = var.db_private_subnet_ids
   tags = {
     Name        = "${var.project_name}-${var.environment}-db-subnet-grp"
     Environment = var.environment
