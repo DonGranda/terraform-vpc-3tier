898eef670fd255e84e700301922caf53939e5506
DonGranda <lesliekofiameg@gmail.com>
2025-06-20 19:55:57 +0000

    output.tf fomatted and  added app_private_subnets_ids app_private_subnet_ids_map app_private_subnets

--- modules/web_server/main.tf
+++ modules/web_server/main.tf
@@ -1,26 +1,26 @@
 resource "aws_instance" "web_server" {
-    for_each = var.app_private_subnets_ids_map
-# This module creates web server instances in the specified private subnets
-  ami = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = each.value
+  for_each = var.app_private_subnets_ids_map
+  # This module creates web server instances in the specified private subnets
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = each.value
   vpc_security_group_ids = [var.app_security_group_id]
 
 
-   tags = {
+  tags = {
     Name        = "${var.project_name}-${var.environment}-web-${each.key}"
     Environment = var.environment
     Project     = var.project_name
     Tier        = "App"
-  
+
   }
   lifecycle {
     create_before_destroy = true
   }
 
 
-#     user_data = base64encode(templatefile("${path.module}/user_data.sh", {
-#     project_name = var.project_name
-#     environment  = var.environment
-#   }))
+  #     user_data = base64encode(templatefile("${path.module}/user_data.sh", {
+  #     project_name = var.project_name
+  #     environment  = var.environment
+  #   }))
 }
--- modules/web_server/variables.tf
+++ modules/web_server/variables.tf
@@ -1,11 +1,11 @@
 variable "app_private_subnets_ids_map" {
-    description = "Map of App private subnet IDs with keys"
-    type        = map(string)
+  description = "Map of App private subnet IDs with keys"
+  type        = map(string)
 }
 
 variable "instance_type" {
-    description = "Instance type for the web server"
-    type        = string 
+  description = "Instance type for the web server"
+  type        = string
 }
 
 # Project name for resource naming
@@ -21,6 +21,6 @@ variable "environment" {
 }
 
 variable "app_security_group_id" {
-    description = "Security group ID for the web server"
-    type        = string
+  description = "Security group ID for the web server"
+  type        = string
 }
