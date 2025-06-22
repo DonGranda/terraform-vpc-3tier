7936b7aef80ec27186144964a2c30469db490e12
DonGranda <lesliekofiameg@gmail.com>
2025-06-22 00:46:25 +0000

    web module add to the root module

--- main.tf
+++ main.tf
@@ -27,19 +27,20 @@ module "security_groups" {
 
   project_name = var.project_name
   environment  = var.environment
+  depends_on   = [module.vpc]
 
 }
 
 module "rds_db" {
-  source = "./modules/db"
-  project_name         = var.project_name
-  environment          = var.environment
+  source       = "./modules/db"
+  project_name = var.project_name
+  environment  = var.environment
 
   # Database configuration
-  db_engine            = var.db_engine
-  db_engine_version = var.db_engine_version
-  db_instance_class        = var.db_instance_class
-  db_security_group_id = module.security_groups.db_security_group_id
+  db_engine             = var.db_engine
+  db_engine_version     = var.db_engine_version
+  db_instance_class     = var.db_instance_class
+  db_security_group_id  = module.security_groups.db_security_group_id
   db_private_subnet_ids = module.subnets.db_private_subnets_ids
 
   # Database storage configuration
@@ -47,12 +48,27 @@ module "rds_db" {
   db_max_allocated_storage = var.db_max_allocated_storage
   db_storage_type          = var.db_storage_type
   db_storage_encrypted     = var.db_storage_encrypted
-  
+
   #db credentials
-db_name = var.db_name
-db_username = var.db_username
+  db_name     = var.db_name
+  db_username = var.db_username
 
+  # depends_on = [
+  #   module.subnets,           
+  #   module.security_groups    
+  # ]
 
-  
 }
+
+module "web_server " {
+  source       = "./modules/web_server"
+  project_name = var.project_name
+  environment  = var.environment
+
+  instance_type               = var.instance_type
+  app_security_group_id       = module.security_groups.app_security_group_id
+  app_private_subnets_ids_map = module.subnets.app_private_subnet_ids_map
+}
+
+
 
