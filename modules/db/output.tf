0538601ecb7baee3ec12e258543d10a5c18834c7
DonGranda <lesliekofiameg@gmail.com>
2025-06-23 18:33:49 +0000

    errors were found and debugged

--- modules/db/output.tf
+++ modules/db/output.tf
@@ -1,22 +1,22 @@
 output "db_instance_id" {
   description = "RDS instance ID"
-  value       = aws_db_instance.main.id
+  value       = aws_db_instance.db-instance.id
 }
 
 
 output "db_instance_endpoint" {
   description = "RDS instance endpoint"
-  value       = aws_db_instance.main.endpoint
+  value       = aws_db_instance.db-instance.endpoint
 }
 
 output "db_instance_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.main.port
+  value       = aws_db_instance.db-instance.port
 }
 
 output "db_subnet_group_name" {
   description = "DB subnet group name"
-  value       = aws_db_subnet_group.main.name
+  value       = aws_db_subnet_group.db-subnet-grp.name
 }
 
 
