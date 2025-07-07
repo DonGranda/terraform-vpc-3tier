56426a8e485ab2802d59d3bc0899baef8499ba84
DonGranda <lesliekofiameg@gmail.com>
2025-07-07 19:14:25 +0000

    formatted output

--- modules/asg/output.tf
+++ modules/asg/output.tf
@@ -1,24 +1,24 @@
 output "launch_template_id" {
   description = "ID of the Launch Template"
-  value       = aws_launch_template.web_servers.id
+  value       = aws_launch_template.app_server-template.id
 }
 
 output "launch_template_latest_version" {
   description = "Latest version of the Launch Template"
-  value       = aws_launch_template.web_servers.latest_version
+  value       = aws_launch_template.app_server-template.latest_version
 }
 
 output "asg_name" {
   description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.web_servers.name
+  value       = aws_autoscaling_group.app-server-asg.name
 }
 
 output "asg_arn" {
   description = "ARN of the Auto Scaling Group"
-  value       = aws_autoscaling_group.web_servers.arn
+  value       = aws_autoscaling_group.app-server-asg.arn
 }
 
 output "asg_id" {
   description = "ID of the Auto Scaling Group"
-  value       = aws_autoscaling_group.web_servers.id
+  value       = aws_autoscaling_group.app-server-asg.id
 }
